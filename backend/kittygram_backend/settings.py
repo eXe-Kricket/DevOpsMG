04c1de1680c4baa2a0301195dcd40d78489ae56a
eXe-Kricket <daimio2008@yandex.ru>
2026-01-17 22:41:41 +0500

    fix_reviu

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -64,7 +64,7 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+        'HOST': os.getenv('POSTGRES_HOST', 'http://89.169.179.0:9003'),
         'PORT': os.getenv('POSTGRES_PORT', '5432'),
 
     }
