efc0802ed91a2524cd5cf1eb92e889fe448d594d
eXe-Kricket <daimio2008@yandex.ru>
2026-01-17 22:30:09 +0500

    fix_reviu

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -5,7 +5,7 @@ from django.core.management.utils import get_random_secret_key
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-example-key')
+SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
 DEBUG = os.getenv('DEBUG', 'false').strip().lower() == 'true'
 
