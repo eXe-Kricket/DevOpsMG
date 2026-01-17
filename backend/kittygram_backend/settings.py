ff2ba26f14a46fab80d7cb4e5ac6bcdce8525287
eXe-Kricket <daimio2008@yandex.ru>
2026-01-17 22:27:56 +0500

    fix_reviu

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -5,11 +5,14 @@ from django.core.management.utils import get_random_secret_key
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-example-key')
 
 DEBUG = os.getenv('DEBUG', 'false').strip().lower() == 'true'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv(
+    'ALLOWED_HOSTS',
+    '*'
+).split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -61,8 +64,9 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', 'postgres'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
+
     }
 }
 
